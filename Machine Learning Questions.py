be0c6ef318da7bc517390fa7d993383ddc1071a5
Hattory <pyrion@mail.ru>
2019-03-26 11:51:47 +0500

    Start work with NapoleonIT Lesson2 material

--- Machine Learning Questions.py
+++ Machine Learning Questions.py
@@ -7,10 +7,18 @@ import mglearn
 
 from sklearn.datasets import load_iris
 from sklearn.datasets import load_breast_cancer
+from sklearn.datasets import fetch_20newsgroups
 from sklearn.model_selection import train_test_split
 
 from sklearn.neighbors import KNeighborsClassifier
+#add words frequency(CountVectorizer) and weighted word frequencies(TfidfVectorizer)
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
+from sklearn.cluster.hierarchical import AgglomerativeClustering                        
+from sklearn.cluster import KMeans
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_val_score
     
     
 def SpeedyNumPyCheck():
@@ -90,6 +98,18 @@ def TrainTestSplit():
     
     print(f"Forecasts on the test case: {clf.predict(X_test)}")
     print("Correctness on the test case: {:.2f}".format(clf.score(X_test, y_test)))
+    
+    fig, axes = plt.subplot(1,3,figsize=(10, 3))
+    
+    for n_neighbors, ax in zip([1, 3, 9], axes):
+        #creating classified object 
+        clf = KNeighborsClassifier(n_neighbors=n_neighbors).fit(X, y)
+        mglearn.plots.plot_2d_separator(clf, X, fill=True, eps=0.5, ax=ax, alpha=.4)
+        mglearn.discrete_scatter(X[:, 0], X[:, 1], y, ax=ax)
+        ax.set_title(f"Neighbors count: {n_neighbors}")
+        ax.set_xlabel("Zero Sign")
+        ax.set_ylabel("First Sign")
+    axes[0].legend(loc=3)
 
 def GenerateDataCase():
     #generate data case
@@ -105,12 +125,97 @@ def GenerateDataCase():
     plt.ylim(-3, 3)
     plt.xlabel("Mark")
     plt.ylabel("Target Variable")
+ 
+class NapoleonIT:
+    
+    def TextClasterization(self):
+        #train_all = fetch_20newsgroups(subset='train')
+        #print (train_all.target_names)
+        
+        #simple_dataset = fetch_20newsgroups(subset = 'train',
+         #   categories = ['comp.sys.mac.hardware', 'soc.religion.christian',
+          #                'rec.sport.hockey'])
+        #print(simple_dataset.data[0]) #Show first letter
+        #print(simple_dataset.data[-1]) #Show last letter
+        #print(simple_dataset.data[-2]) #Show penultimate letter
+        
+        #simple_dataset.target # letter subject
+        
+        #print( len(simple_dataset.data)) #all object in sample
+        
+        #Set min/max border
+        vectorizer = CountVectorizer(max_df=500, min_df=10)
+        
+        #Discard unnecessary word(cheate object-sign matrix)
+        
+        #matrix = vectorizer.fit_transform(simple_dataset.data)
+        #print(matrix.shape)
+        
+        #list of sign
+        #print(vectorizer.get_feature_names())
+        
+        #Use NeighbourJoining:        
+        #Create cosine measure
+        #model = AgglomerativeClustering(n_clusters=3, affinity='cosine',
+         #                              linkage = 'complete')
+        #preds = model.fit_predict(matrix.toarray())
+        
+        #print( list(preds))
+        
+        #Use KMeans method
+        #use random state to reproducibility of results
+        #model = KMeans(n_clusters=3, random_state=1)
+        #preds = model.fit_predict(matrix.toarray())
+        
+        #clusterization results
+        #print(preds)
+        #True answer
+        #print(simple_dataset.target)
+        
+        #we made a right predictions
+        #if we change the numbering of the clusters
+        #mapping = {2: 1, 1: 2, 0: 0}
+        #mapped_preds = [mapping[pred] for pred in preds]
+        #we can calculate wrong share of predictions
+        #and accurance ~33%
+        #print( float(sum(mapped_preds != simple_dataset.target)) / 
+         #      len(simple_dataset.target))
+        #for compression we can take another classifier
+        #clf = LogisticRegression()
+        #print( cross_val_score(clf, matrix, simple_dataset.target).mean() )
+        
+        #Now take 3 similar theme
+        dataset = fetch_20newsgroups(
+                subset='train',
+                categories=['comp.sys.mac.hardware',
+                            'comp.os.ms-windows.misc',
+                            'comp.graphics'])
+        #use K-means method
+        matrix = vectorizer.fit_transform(dataset.data)
+        model = KMeans(n_clusters=3, random_state=42)
+        preds = model.fit_predict(matrix.toarray())
+        print(preds)
+        print(dataset.target)
+        
+        #we see how alghorithm quality was dropped
+        mapping = {2: 0, 1: 1, 0: 2}
+        mapped_preds = [mapping[pred] for pred in preds]
+        #accurance ~33%
+        print( float(sum(mapped_preds != dataset.target)) / len(dataset.target))
+        
+        #for compression we can take another classifier
+        clf = LogisticRegression()
+        print (cross_val_score(clf, matrix, dataset.target).mean() )
+        
     
 def main():
     #SpeedyNumPyCheck()
     #IrisClassification()
     #CancerClassification()
-    TrainTestSplit()
+    #TrainTestSplit()
+    lection_2 = NapoleonIT()
+    lection_2.TextClasterization()
+    
     
     
 if __name__ == '__main__':
